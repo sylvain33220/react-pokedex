40b5e0b705803fc8ce697081a1471d7c6ef50a2d
sylvain33220 <sylvain.poteaux@gmail.com>
2024-06-12 14:24:13 +0200

    components.2

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,9 +1,21 @@
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 function PokemonCard () {
+    const pokemon = pokemonList[0]
     return(
         <div>
             <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-                <figcaption>Bulbasaur</figcaption>
+               {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>????</p>}
+                <figcaption>{pokemonList.name}</figcaption>
             </figure>
         </div>
     ) 
